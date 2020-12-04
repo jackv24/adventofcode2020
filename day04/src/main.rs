a5071b140d1555ed69813c928ac972e8b2499309
Jack Vine <jackv24@users.noreply.github.com>
2020-12-04 23:09:15 +0000

    Completed day 4 part 2

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,5 +1,5 @@
-use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
+use std::{fs::File, ops::RangeInclusive};
 
 fn main() -> io::Result<()> {
     let required_fields: [(&str, fn(&str) -> bool); 7] = [
@@ -68,36 +68,137 @@ fn main() -> io::Result<()> {
 }
 
 fn validate_byr(value: &str) -> bool {
+    validate_year_range(value, 1920..=2002)
+}
+
+fn validate_iyr(value: &str) -> bool {
+    validate_year_range(value, 2010..=2020)
+}
+
+fn validate_eyr(value: &str) -> bool {
+    validate_year_range(value, 2020..=2030)
+}
+
+fn validate_hgt(value: &str) -> bool {
+    let num = value
+        .chars()
+        .take_while(|c| c.is_numeric())
+        .collect::<String>()
+        .parse::<i32>()
+        .expect("Couldn't parse num");
+
+    if value.ends_with("cm") {
+        (150..=193).contains(&num)
+    } else if value.ends_with("in") {
+        (59..=76).contains(&num)
+    } else {
+        false
+    }
+}
+
+fn validate_hcl(value: &str) -> bool {
+    if value.chars().count() != 7 {
+        return false;
+    }
+    let mut tested_first = false;
+    for c in value.chars() {
+        if !tested_first {
+            if c != '#' {
+                return false;
+            }
+            tested_first = true;
+        } else {
+            let is_num = ('0'..='9').contains(&c);
+            let is_char = ('a'..='f').contains(&c);
+            if !is_num && !is_char {
+                return false;
+            }
+        }
+    }
+
+    true
+}
+
+fn validate_ecl(value: &str) -> bool {
+    match value {
+        "amb" | "blu" | "brn" | "gry" | "grn" | "hzl" | "oth" => true,
+        _ => false,
+    }
+}
+
+fn validate_pid(value: &str) -> bool {
+    if value.chars().count() != 9 {
+        return false;
+    }
+    for c in value.chars() {
+        if !c.is_numeric() {
+            return false;
+        }
+    }
+
+    true
+}
+
+fn validate_year_range(value: &str, range: RangeInclusive<i32>) -> bool {
     if value.chars().count() != 4 {
         return false;
     }
 
     match value.parse::<i32>() {
-        Ok(num) => (1920..=2002).contains(&num),
+        Ok(num) => range.contains(&num),
         Err(_) => false,
     }
 }
 
-fn validate_iyr(value: &str) -> bool {
-    true // TODO
+#[test]
+fn byr_valid() {
+    assert_eq!(validate_byr("2002"), true);
 }
 
-fn validate_eyr(value: &str) -> bool {
-    true // TODO
+#[test]
+fn byr_invalid() {
+    assert_eq!(validate_byr("2003"), false);
 }
 
-fn validate_hgt(value: &str) -> bool {
-    true // TODO
+#[test]
+fn hgt_valid() {
+    assert_eq!(validate_hgt("60in"), true);
+    assert_eq!(validate_hgt("190cm"), true);
 }
 
-fn validate_hcl(value: &str) -> bool {
-    true // TODO
+#[test]
+fn hgt_invalid() {
+    assert_eq!(validate_hgt("190in"), false);
+    assert_eq!(validate_hgt("190"), false);
 }
 
-fn validate_ecl(value: &str) -> bool {
-    true // TODO
+#[test]
+fn hcl_valid() {
+    assert_eq!(validate_hcl("#123abc"), true);
 }
 
-fn validate_pid(value: &str) -> bool {
-    true // TODO
+#[test]
+fn hcl_invalid() {
+    assert_eq!(validate_hcl("#123abz"), false);
+    assert_eq!(validate_hcl("123abc"), false);
+}
+
+#[test]
+fn ecl_valid() {
+    assert_eq!(validate_ecl("brn"), true);
+}
+
+#[test]
+fn ecl_invalid() {
+    assert_eq!(validate_ecl("wat"), false);
+}
+
+#[test]
+fn pid_valid() {
+    assert_eq!(validate_pid("000000001"), true);
+}
+
+#[test]
+fn pid_invalid() {
+    assert_eq!(validate_pid("0123456789"), false);
 }
