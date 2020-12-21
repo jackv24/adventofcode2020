db48baae9728e96774dc43d8dc41853d1bd3b1b8
Jack Vine <jackv24@users.noreply.github.com>
2020-12-21 10:29:57 +0000

    Committing old half done day 11 part 2

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -62,7 +62,7 @@ fn execute_round(chars: &Vec<char>, char_buf: &mut Vec<char>, width: usize) -> b
                 new_c = '#';
             }
         } else if c == '#' {
-            if count_adjacent(&chars, width, x, y, &'#') >= 4 {
+            if count_adjacent(&chars, width, x, y, &'#') >= 5 {
                 new_c = 'L';
             }
         }
@@ -86,62 +86,89 @@ fn get_2d<T>(vec: &Vec<T>, width: usize, x: usize, y: usize) -> &T {
 }
 
 fn count_adjacent(vec: &Vec<char>, width: usize, x: usize, y: usize, value: &char) -> usize {
-    let max_x = width - 1;
-    let max_y = (vec.len() / width) - 1;
-
     let mut occupied_count = 0;
 
-    if y > 0 {
-        // Up
-        if get_2d(vec, width, x, y - 1) == value {
-            occupied_count += 1;
-        }
-        if x > 0 {
-            // Up-left
-            if get_2d(vec, width, x - 1, y - 1) == value {
-                occupied_count += 1;
-            }
-        }
-        if x < max_x {
-            // Up-right
-            if get_2d(vec, width, x + 1, y - 1) == value {
-                occupied_count += 1;
-            }
-        }
+    // Right
+    if can_see(vec, width, x, y, 1, 0, value) {
+        occupied_count += 1;
     }
 
-    if x > 0 {
-        // Left
-        if get_2d(vec, width, x - 1, y) == value {
-            occupied_count += 1;
-        }
-        if y < max_y {
-            // Down-left
-            if get_2d(vec, width, x - 1, y + 1) == value {
-                occupied_count += 1;
-            }
-        }
+    // Right-Up
+    if can_see(vec, width, x, y, 1, -1, value) {
+        occupied_count += 1;
     }
 
-    if y < max_y {
-        // Down
-        if get_2d(vec, width, x, y + 1) == value {
-            occupied_count += 1;
-        }
-        if x < max_x {
-            // Down-right
-            if get_2d(vec, width, x + 1, y + 1) == value {
-                occupied_count += 1;
-            }
-        }
+    // Up
+    if can_see(vec, width, x, y, 0, -1, value) {
+        occupied_count += 1;
     }
 
-    if x < max_x {
-        // Right
-        if get_2d(vec, width, x + 1, y) == value {
-            occupied_count += 1;
-        }
+    // Up-Left
+    if can_see(vec, width, x, y, -1, -1, value) {
+        occupied_count += 1;
+    }
+
+    // Left
+    if can_see(vec, width, x, y, -1, 0, value) {
+        occupied_count += 1;
+    }
+
+    // Left-Down
+    if can_see(vec, width, x, y, -1, 1, value) {
+        occupied_count += 1;
+    }
+
+    // Down
+    if can_see(vec, width, x, y, 0, 1, value) {
+        occupied_count += 1;
+    }
+
+    // Down-Right
+    if can_see(vec, width, x, y, 1, 1, value) {
+        occupied_count += 1;
     }
 
     occupied_count
+}
+
+fn can_see(
+    vec: &Vec<char>,
+    width: usize,
+    x: usize,
+    y: usize,
+    move_x: i32,
+    move_y: i32,
+    value: &char,
+) -> bool {
+    let max_x = width - 1;
+    let max_y = (vec.len() / width) - 1;
+
+    if move_x < 0 {
+        if x == 0 {
+            return false;
+        }
+    } else {
+        if x >= max_x {
+            return false;
+        }
+    }
+
+    if move_y < 0 {
+        if y == 0 {
+            return false;
+        }
+    } else {
+        if y >= max_y {
+            return false;
+        }
+    }
+
+    let new_x = (x as i32 + move_x) as usize;
+    let new_y = (y as i32 + move_y) as usize;
+
+    if get_2d(vec, width, new_x, new_y) == value {
+        true
+    } else {
+        can_see(vec, width, new_x, new_y, move_x, move_y, value)
+    }
 }
